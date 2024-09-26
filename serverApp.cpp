5958ef8f75c7fe18a1c4dd97e5db5d1b81808b0d
nuno <nunocapela.154@gmail.com>
2024-09-26 18:02:40 +0100

    project setup, sem desenvolvimentos

---
+++ serverApp.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <interface.h>
+#include <warehouseWebServer.h>
+#include <niDAQWebInterface.h>
+#include <localControl.h>
+
+int main() {
+	printf("Welcome to Intelligent Supervision\n");
+	printf("Press any key to start\n");
+
+	configure_simulator_server();
+	start_mg_server();
+	initializeHardwarePorts();
+
+	int keyboard = 0;
+
+	showLocalMenu();
+
+	while (keyboard != 27) {  // 27 is the ASCII code for ESC
+		if (_kbhit()) {
+			keyboard = _getch();
+			executeLocalControl(keyboard);
+		}
+		else {
+			keyboard = 0;
+		}
+		Sleep(1);
+	}
+
+	stop_mg_server();
+
+	return 0;
+}
